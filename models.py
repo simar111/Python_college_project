dcb1e5bba8a10247bbcbf0142458fcdf186a8c8d
simar111 <simarnarula1477@gmail.com>
2025-09-25 04:17:18 -0700

    done

---
+++ models.py
@@ -0,0 +1,10 @@
+class Book:
+    def __init__(self, title, author, available=True):
+        self.title = title
+        self.author = author
+        self.available = available
+
+class Student:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
